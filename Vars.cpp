e9f4695488101bfdcbff52706d950ff9285d61d2
Kaitlin <kaitlin.dechastelainfinnigan@gmail.com>
2018-07-23 23:54:59 -0600

    added vars file

---
+++ Vars.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+  int n1 = 0; //Declaring an integer
+  int n2 = 1;
+
+  std:: cout << "Enter first integer:\n";
+  std:: cin >> n1;
+
+  std:: cout << "Enter Second integer\n";
+  std:: cin >> n2;
+
+  int sum = n1 +n2;
+  std:: cout << "Sum: " << sum <<std::endl;
+
+}
